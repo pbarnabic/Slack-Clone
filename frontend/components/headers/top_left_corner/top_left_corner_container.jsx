2e13ff09fdabc5fe4adc4e652e1b0894373e0b05
Paul Barnabic <ptbarnabic@gmail.com>
2018-11-27 00:39:10 -0500

    Fixed missing exception

--- frontend/components/headers/top_left_corner/top_left_corner_container.jsx
+++ frontend/components/headers/top_left_corner/top_left_corner_container.jsx
@@ -5,7 +5,7 @@ import {logout} from '../../../actions/session_actions'
 
 const mapStateToProps = (state, ownProps) => {
   return{
-    currentUser: state.entities.users[state.session.id],
+    currentUser: state.entities.users[state.session.id] || {username: "",id:-1},
   };
 };
 
