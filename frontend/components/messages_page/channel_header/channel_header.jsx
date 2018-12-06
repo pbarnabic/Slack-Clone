b14484a094edf3dff5f30c9b1d8f5e9f46876f69
Paul Barnabic <ptbarnabic@gmail.com>
2018-12-05 23:30:30 -0500

    Updated Channel Header to Indicate reflect whether the current messages belonged to a channel or direct message. Furthermore, the name that appears at the top of the page are now the same as the recently modified dm list names

--- frontend/components/messages_page/channel_header/channel_header.jsx
+++ frontend/components/messages_page/channel_header/channel_header.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 export default class MessagePage extends React.Component{
 
-  componentDidMount(){
-    // this.props.fetchChannels();
-  }
-
   render(){
-
+    
+    let channelName = this.parseChannelName(this.props.channel);
+    if(this.props.channel.is_direct_message == false){
+      channelName = "#" + channelName;
+    }
 
     return(
     <div>
       <div className="top-row-left-side-top-left">
-        <span> #{this.props.channel.channel_name} </span>
+        <span>{channelName} </span>
       </div>
       <div className="bottom-row-left-side-top-left">
         <span>👤 {this.props.channel.userIds.length}</span>
@@ -20,6 +20,47 @@ export default class MessagePage extends React.Component{
     </div>
 
   );
+
+  }
+
+  parseChannelName(channel){
+    let currentUserUsername = this.props.currentUser.username;
+    let originalChannelName = channel.channel_name;
+
+    let startIndex;
+    let endIndex;
+    let name = "";
+
+    if(originalChannelName == currentUserUsername){
+      return originalChannelName;
+    }
+
+    if(originalChannelName.indexOf(currentUserUsername + ",") != -1 ){
+      startIndex = originalChannelName.indexOf(currentUserUsername + ",");
+      endIndex = startIndex + currentUserUsername.length + 1;
+      name = originalChannelName.slice(0,startIndex) + originalChannelName.slice(endIndex);
+
+    }else if(originalChannelName.indexOf(currentUserUsername) != -1){
+
+      if(originalChannelName.indexOf(currentUserUsername) == originalChannelName.length - currentUserUsername){
+        startIndex = 0;
+        endIndex = originalChannelName.indexOf(currentUserUsername);
+        name = originalChannelName.slice(0,endIndex);
+      }else{
+        return originalChannelName;
+      }
+
+    }else{
+      return originalChannelName;
+    }
+
+    name.indexOf(",,") != -1 ? name = name.slice(0,startIndex) + name.slice(startIndex+1) : name;
+
+    name.slice(name.length - 1) == "," ? name = name.slice(0,name.length - 1) : name;
+
+    name.slice(0,1) == "," ? name = name.slice(1) : name;
+
+    return name;
 
   }
 
--- frontend/components/messages_page/channel_header/channel_header_container.jsx
+++ frontend/components/messages_page/channel_header/channel_header_container.jsx
@@ -7,15 +7,11 @@ import ChannelHeader from "./channel_header";
 const mapStateToProps = (state, ownProps) => {
 
   return{
-    channel: state.entities.channels[ownProps.channel_id] || state.entities.directMessages[ownProps.channel_id] || {channel_name: "", userIds:{length: 0}},
-    channel_id: ownProps.channel_id
+    channel: state.entities.channels[ownProps.channel_id] || state.entities.directMessages[ownProps.channel_id] || {channel_name: "", userIds:{length: 0}, is_direct_message: false},
+    channel_id: ownProps.channel_id,
+    currentUser: state.entities.users[state.session.id] || {id: "", username:""},
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return{
-    fetchChannels: () => dispatch(fetchChannels())
-  };
-};
 
-export default connect(mapStateToProps,mapDispatchToProps)(ChannelHeader);
+export default connect(mapStateToProps,{})(ChannelHeader);
