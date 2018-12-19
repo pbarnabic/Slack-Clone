e8266f8fe30091e65bb09a0445d5ab05b9d38119
Paul Barnabic <ptbarnabic@gmail.com>
2018-12-19 16:57:54 -0500

    Got rid of random divs and constants

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
-import {API_WS_ROOT} from '../constants/constants.js';
 import { ActionCableProvider } from 'react-actioncable-provider';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <div>
         <ActionCableProvider url={'/cable'}>
           <App />
         </ActionCableProvider>
-      </div>
     </HashRouter>
   </Provider>
 );
